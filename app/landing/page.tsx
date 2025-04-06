f3b844f442a00d3f0c2c8420b4f69dfc7d921b84
Akeelabbas29 <akeelabbas29@gmail.com>
2025-04-06 23:49:41 +0530

    featuresecrion

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 // import FeatureSection from "./feature-section";
-import HeroSection from "./feature-section";
+import FeatureSection from "./FeatureSection";
 // import HeroSection from "./feature-section2";
-
+import ApartmentSelection from "./ApartmentSelection";
+import DummySection from "./DummySection";
 const page = () => {
   return (
-    <div>
-      <HeroSection />
+    <div className="overflow-x-hidden">
+      {/* Section 1 */}
+      <FeatureSection />
+      {/* <ApartmentSelection /> */}
+      {/* <DummySection /> */}
     </div>
   );
 };
